78d1815cbe4b9cdca30fbd96474a500252e8b93e
fidhalkotta <fidhalkotta@gmail.com>
2023-05-13 12:57:45 +0100

    Add example files and update README.md

--- README.md
+++ README.md
@@ -14,8 +14,17 @@ I have extended the environment, by developing my own fault model, which allows 
 
 I have also integrated a continuous wind disturbance model that allows for a more realistic and complex simulation environment.
 
+### Example Files (Useful for UCL markers)
+
+The folder `example_files` contains a few examples of the different aspects of the project. Including the following files:
+
+- `ModelB.2.py` - This is the custom OpenAI Gym environment that I have created. It is based on the `gym-copter` environment, but I have added my own fault model and wind model.
+- `trainModelB.2.py` - This is the file that trains the PPO model with StableBaselines3 integration, saving a model periodically.
+- `loadModelB.2.py` - This is the file that loads the trained model and visualises the real time simulation, saving the data to a csv file.
+- `plot_ModelB.2.ipynb` - This is a jupyter notebook that processes the data from the csv file and generates the graphs used in the report. An example of the graphs generated can be seen below.
 
 
+<img src="example_files/images/faultyCaseComparison-v2.png" height="600">
 
 ## Future Plans
 
---
+++ example_files/trainModelB.2.py
@@ -0,0 +1,67 @@
+import gym as gym
+from stable_baselines3 import PPO
+import os
+import time
+import numpy as np
+
+
+def runOneRun(fault_cases, f_i):
+    total_runs = 1
+    print(f"I will train {total_runs} runs")
+    print(f"Current faults cases are\n {fault_cases}")
+
+    starting_index = 3
+
+    for run in range(starting_index, total_runs + starting_index):
+        print(f"Training run {run}")
+        project_name = f"ModelB_V3_1," \
+                       f"fmi_{str(f_i).replace('.', '_')}," \
+                       f"run_{run}"
+        models_dir = f"models/{project_name}"
+
+        logdir = "logs"
+
+        if not os.path.exists(models_dir):
+            os.makedirs(models_dir)
+
+        if not os.path.exists(logdir):
+            os.makedirs(logdir)
+
+        env = gym.make("gym_copter:ModelB-v3",
+                       fault_cases=fault_cases,
+                       ) 
+        env.reset()
+
+        print(f"Current faults cases are\n {env.fault_cases}")
+
+        model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=logdir)
+
+        total_timesteps = 5_000_000
+        timesteps = 200_000
+        iterations = int(total_timesteps / timesteps)
+
+        for j in range(1, iterations + 1):
+            model.learn(total_timesteps=timesteps, reset_num_timesteps=False, tb_log_name=project_name)
+            #model.save(f"{models_dir}/{timesteps * j}") # Commented out as this is just an example file
+
+        print(f"Finished run {run}")
+
+    print(f"Finished Training")
+
+
+def main():
+    f_i = 0.65
+
+    fault_cases = [
+            [1, 1, 1, 1],
+            [f_i, 1, 1, 1],
+            [1, f_i, 1, 1],
+            [1, 1, f_i, 1],
+            [1, 1, 1, f_i],
+    ]
+
+    runOneRun(fault_cases, f_i)
+
+
+if __name__ == '__main__':
+    main()
